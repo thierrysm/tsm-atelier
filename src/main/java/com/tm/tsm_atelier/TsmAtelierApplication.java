6eccd8c3b663a573ec3bc9303727f036b6d87c95
Thierry <thierrydev48@gmail.com>
2025-06-17 09:08:29 -0300

    create project

---
+++ src/main/java/com/tm/tsm_atelier/TsmAtelierApplication.java
@@ -0,0 +1,13 @@
+package com.tm.tsm_atelier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TsmAtelierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TsmAtelierApplication.class, args);
+	}
+
+}
