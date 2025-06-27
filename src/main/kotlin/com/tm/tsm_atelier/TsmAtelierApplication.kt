9c15c428792d20a281a58573474cdef5a1e9b8ad
Thierry <thierrydev48@gmail.com>
2025-06-27 10:53:16 -0300

    create project

---
+++ src/main/kotlin/com/tm/tsm_atelier/TsmAtelierApplication.kt
@@ -0,0 +1,11 @@
+package com.tm.tsm_atelier
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TsmAtelierApplication
+
+fun main(args: Array<String>) {
+	runApplication<TsmAtelierApplication>(*args)
+}
