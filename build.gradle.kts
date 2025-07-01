7081ac4bc9c215b9e3a0f58865e6f9c82e324958
Thierry Marinho <thierrydev48@gmail.com>
2025-06-30 22:03:39 -0300

    test: adiciona o framework Kotest ao projeto

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,9 @@ dependencies {
     implementation("org.flywaydb:flyway-database-postgresql")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+    testImplementation("io.kotest:kotest-assertions-core:5.9.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
 }
 
 kotlin {
